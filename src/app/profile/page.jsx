0364d5e9fe74ced41e9505b90e3571a9799c4f58
ysilksa <isabella.yan.business@gmail.com>
2025-12-22 12:17:02 -0600

    add profile page styling

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -18,7 +18,20 @@ const ProfilePage = () => {
                     <h2>Change username</h2>
                     <p>This is how you will show up on posts and comments.</p>
                     <TextField/>
-                    <Button><h2>Update</h2></Button>
+                    <Button
+                        type="submit"
+                        sx = {{
+                            width: "100%", 
+                            backgroundColor: "#828181",
+                            color: "#FFFFFC",
+                            padding: "16px",
+                            marginTop: "16px",
+                            fontFamily: "var(--font-inria-sans)"
+                        }}
+                        variant="contained"
+                    >
+                        <h2>Update</h2>
+                    </Button>
                 </div>
 
                 {/* Change password */}
@@ -26,7 +39,20 @@ const ProfilePage = () => {
                     <h2>Change password</h2>
                     <p>Update your password to keep your account secure.</p>
                     <TextField/>
-                    <Button><h2>Update</h2></Button>
+                    <Button
+                        type = "submit"
+                        sx = {{
+                            width: "100%",
+                            backgroundColor: "#828181",
+                            color: "#FFFFFC",
+                            padding: "16px",
+                            marginTop: "16px",
+                            fontFamily: "var(--font-inria-sans)"
+                        }}
+                        variant="contained"
+                    >
+                        <h2>Update</h2>
+                    </Button>
                 </div>
 
 
